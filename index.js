2c4846a203d6cf4a695a4bc6e3f030784697f134
Domingo Cabeza <domingoacd1@gmail.com>
2020-02-27 20:45:25 -0400

    Created basic handlebars usage

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
+const hbs = require('express-handlebars');
+const home = require('./routes/index');
 
 
-app.get('/', (req, res)=> {
-  res.send('test');
-});
+app.set('view engine', 'hbs');
+
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  layoutsDir: __dirname + '/views/layouts',
+  partialsDir: __dirname + '/views/partials'
+}));
+
+app.use('/', home);
+// app.get('/', (req, res)=> {
+//   res.send('test');
+// });
 
 app.listen(3000, () => {
   console.log('Running on port 3000');
+  console.log(__dirname);
 });
