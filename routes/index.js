e39909e8317c64e290a1868f3c623854ea668b7e
Domingo Cabeza <domingoacd1@gmail.com>
2020-03-16 16:55:53 -0400

    Deleted test line

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,6 @@ const database = require('../database/connect');
 
 router.get('/', async (req, res, next) => {
     res.render('home');
-    await database.query('INSERT INTO clients (full_name, license_number, phone_number) VALUES ("domingo2", 010101, 2123366)', (err, rows, fields) => {
-        if (err) throw err;
-    });
 });
 
 module.exports = router;
