fd6f63a8811cf857a48a9d7d7bd42e4f938d1d59
Domingo Cabeza <domingoacd1@gmail.com>
2020-03-17 15:41:23 -0400

    WIP - Creating signup view

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ const database = require('../database/connect');
 router.get('/', async (req, res, next) => {
     res.render('home');
 });
-
+router.get('/signup', (req, res, next) => {
+    res.render('signup');
+});
 module.exports = router;
