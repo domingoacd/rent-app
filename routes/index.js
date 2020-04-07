806630402c749b3260de007dea026e866be3f889
Domingo Cabeza <domingoacd1@gmail.com>
2020-04-07 19:50:22 -0400

    Added redirect after successful signup

--- routes/index.js
+++ routes/index.js
@@ -12,24 +12,34 @@ router.route('/signup')
     })
     .post(async (req, res, next) => {
         const {account_type} = req.body;
-        console.log(account_type)
-
+        
         if (account_type == 'user') {
             await database.query(`INSERT INTO users (full_name, email, password) VALUES ("${req.body.name}", "${req.body.email}", "${req.body.password}")`, (err, result) => {
-                if (err) throw err;
-                console.log('Inserted!');
+                if (err) {
+                    throw err;       
+                } else {
+                    res.redirect(`/registration_success?account=${account_type}&name=${req.body.name}`);
+                }
+                
             });
-
+            
         } else if (account_type == 'client') {
             await database.query(`INSERT INTO clients (full_name, license_number, phone_number) VALUES ("${req.body.name}", "${req.body.license}", "${req.body.phone}")`, (err, result) => {
                 if (err) {
                     throw err;
                 };
-                console.log('Inserted client!');
+                // res.render('registration_success', account_type);
             });
         } else {
             throw 'ERROR - Account type can just be either user or client';
         }
-        res.send('<h1>Inserted!</h1>');
+        // res.render('registration_success');
     });
+
+router.route('/registration_success').get((req, res, next) => {
+    res.render('registration_success', {
+        account_type: req.query.account,
+        name: req.query.name
+    });
+});
 module.exports = router;
