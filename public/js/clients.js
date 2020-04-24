deb6f3022e68619167cafd3fa52c58b3621e823e
Domingo Cabeza <domingoacd1@gmail.com>
2020-04-24 10:49:13 -0400

    Adding styles to client panel

--- public/js/clients.js
+++ public/js/clients.js
@@ -1,5 +1,6 @@
 const btnAddClient = document.querySelector('.j-addClient');
 const addClientModal = document.querySelector('.j-modal-addClient');
+const btnSaveClient = document.querySelector('.j-saveClient');
 
 function closeAddClientModal(e) {
   const clieckedTarget = e.target;
@@ -8,9 +9,26 @@ function closeAddClientModal(e) {
   }
 }
 
+function saveClient(e) {
+  e.preventDefault();
+  const url = '/async/saveClient';
+  const data =
+  fetch(url, {
+    body: {
+      full_name: document.querySelector('#name').value,
+      license: document.querySelector('#license').value,
+      phone_number: document.querySelector('#phone').value
+    }
+  })
+  .then(res => res.json())
+  .catch(error => console.error(error))
+  .then(data => console.log(data))
+}
+
 function showAddClientModal(e) {
   addClientModal.classList.add('show');
 }
 
 btnAddClient.addEventListener('click', showAddClientModal);
+btnSaveClient.addEventListener('click', saveClient);
 addClientModal.addEventListener('click', closeAddClientModal);
