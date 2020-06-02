87ce8b63ca68b299c6d6c799dae5127f7756ff73
Domingo Cabeza <domingoacd1@gmail.com>
2020-06-02 11:10:00 -0400

    Changed registration and sign in view

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,6 @@ router
     res.render('signup');
   })
   .post(async (req, res, next) => {
-    const { account_type } = req.body;
-
-    if (account_type == 'user') {
       await database.query(
         `INSERT INTO users (full_name, email, password) VALUES ("${req.body.name}", "${req.body.email}", "${req.body.password}")`,
         (err, result) => {
@@ -22,28 +19,11 @@ router
             throw err;
           } else {
             res.redirect(
-              `/registration_success?account=${account_type}&name=${req.body.name}`
+              `/registration_success?name=${req.body.name}`
             );
           }
         }
       );
-    } else if (account_type == 'client') {
-      await database.query(
-        `INSERT INTO clients (full_name, license_number, phone_number) VALUES ("${req.body.name}", "${req.body.license}", "${req.body.phone}")`,
-        (err, result) => {
-          if (err) {
-            throw err;
-          } else {
-            res.redirect(
-              `/registration_success?account=${account_type}&name=${req.body.name}`
-            );
-          }
-          // res.render('registration_success', account_type);
-        }
-      );
-    } else {
-      throw 'ERROR - Account type can just be either user or client';
-    }
     // res.render('registration_success');
   });
 
